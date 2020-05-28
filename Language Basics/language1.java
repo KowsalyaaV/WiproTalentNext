c9362589b64d6838529f70aacfcfc801a138d958
KowsalyaaV <58818682+KowsalyaaV@users.noreply.github.com>
2020-05-28 22:55:48 +0530

    Add files via upload

---
+++ Language Basics/language1.java
@@ -0,0 +1,10 @@
+
+public class language1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0] + " Technologies " + args[1]);
+
+	}
+
+}
